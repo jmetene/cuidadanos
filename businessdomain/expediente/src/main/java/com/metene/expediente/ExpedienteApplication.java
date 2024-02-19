858b2e28a373999ce8c9188b0204e30b7e690ebe
USERSAD\jmetenen <juan.metenenguema@externos.correo.gob.es>
2024-02-20 00:20:03 +0100

    Modificación en los microservicios

---
+++ businessdomain/expediente/src/main/java/com/metene/expediente/ExpedienteApplication.java
@@ -0,0 +1,11 @@
+package com.metene.expediente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpedienteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExpedienteApplication.class, args);
+    }
+}
