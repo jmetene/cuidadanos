27bec908ba428cef9604c7aea193342f02da90b3
USERSAD\jmetenen <juan.metenenguema@externos.correo.gob.es>
2024-03-03 20:26:20 +0100

    Refactorización código

--- businessdomain/expediente/src/main/java/com/metene/expediente/ExpedienteApplication.java
+++ businessdomain/expediente/src/main/java/com/metene/expediente/ExpedienteApplication.java
@@ -2,8 +2,10 @@ package com.metene.expediente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExpedienteApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExpedienteApplication.class, args);
--- businessdomain/expediente/src/main/java/com/metene/expediente/config/WebClientConfig.java
+++ businessdomain/expediente/src/main/java/com/metene/expediente/config/WebClientConfig.java
@@ -1,11 +1,14 @@
 package com.metene.expediente.config;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
@@ -13,29 +16,45 @@ import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
-    @Value(value = "${url.apis.basepath}")
-    private String baseUrlApis;
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebClientConfig.class);
+    private final DiscoveryClient discoveryClient;
+
+    @Value(value = "${cuidadano.service.name}")
+    private String ciudadanoServiceName;
+    @Value(value = "${cuidadanos.api.base-path}")
+    private String cuidadanoAPIBasePath;
     @Value(value = "${apis.timeout}")
     private Long timeoutApi;
 
-    @Bean
-    public WebClient webClientApis() {
-        return createWebClient(baseUrlApis, timeoutApi);
+    public WebClientConfig(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
     }
 
-    private WebClient createWebClient(String baseUrl, Long timeout) {
+    @Bean
+    public WebClient webClientApis() {
+        return createWebClient();
+    }
+
+    private WebClient createWebClient() {
 
         HttpClient httpClient = HttpClient.create()
-                .responseTimeout(Duration.ofMillis(timeout));
+                .responseTimeout(Duration.ofMillis(timeoutApi));
 
         return WebClient.builder()
-                .baseUrl(baseUrl)
+                .baseUrl(getServiceBaseURL().toString())
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .exchangeStrategies(
-                        ExchangeStrategies.builder()
-                                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(500 * 1024))
-                                .build())
                 .build();
+    }
 
+    private StringBuilder getServiceBaseURL() {
+        StringBuilder serviceURI = new StringBuilder();
+        try {
+            ServiceInstance instance = discoveryClient.getInstances(ciudadanoServiceName).get(0);
+            serviceURI.append(instance.getUri().toString());
+        } catch (Exception e) {
+            LOGGER.error("WebClientConfig::getServiceBaseURL:: error {} ", e.getMessage());
+        }
+
+        return serviceURI.append(cuidadanoAPIBasePath);
     }
 }
